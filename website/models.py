601580a97aa5c5737416b4940e00052043c17863
mykytaolin <olin.mykyta@gmail.com>
2024-01-01 16:37:02 +0100

    Solve problem which not allow to create a normal Lead user

--- website/models.py
+++ website/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Leads(models.Model):
 
+class Leads(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age_name = models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", models.SET_NULL, null=True)  # every lead have just 1 agent
 
 
@@ -17,3 +17,5 @@ class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     '''lead = models.ForeignKey("Agent", models.SET_NULL, null=True) every agent has only one lead ->
     it's completely wrong'''
+    def __str__(self):
+        return self.user.email
