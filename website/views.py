bfa8f2ff28ce0fc46bbe5a11988e7f23ea7a069b
mykytaolin <olin.mykyta@gmail.com>
2024-01-05 18:43:23 +0100

    Change HTTPRESPONSE by render in lead_detail function and create a link for going back from lead detail to leads

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Lead
-from django.http import HttpResponse
 
 
 def lead_list(request):
@@ -8,11 +7,12 @@ def lead_list(request):
     context = {
         "leads": leads
     }
-    return render(request, "lead_list.html", context)
+    return render(request, "website/lead_list.html", context)
 
 
 def lead_detail(request, pk):
-    print(pk)
     lead = Lead.objects.get(id=pk)
-    print(lead)
-    return HttpResponse("here is a detail view")
+    context = {
+        "lead": lead
+    }
+    return render(request, "website/lead_detail.html", context)
