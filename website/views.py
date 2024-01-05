b50fe1741a44c1d7d0a3c72fcfdabe8c48a6264a
mykytaolin <olin.mykyta@gmail.com>
2024-01-05 18:30:52 +0100

    I built navigation on our site by primary key for leads details.

--- website/views.py
+++ website/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Lead
+from django.http import HttpResponse
 
 
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
-        "leads": leads,
+        "leads": leads
     }
-    return render(request, "website/lead_list.html", context)
+    return render(request, "lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    return HttpResponse("here is a detail view")
