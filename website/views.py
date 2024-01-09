89f4423f4e319821ea7b08243e16ec098f5401e7
mykytaolin <olin.mykyta@gmail.com>
2024-01-09 20:58:57 +0100

    I made a dynamic references for website urls and create a lead delete function

--- website/views.py
+++ website/views.py
@@ -13,23 +13,37 @@ def lead_list(request):
 
 def lead_update(request, pk):
     lead = Lead.objects.get(id=pk)
-    form = LeadForm
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST, instance=lead)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            lead.first_name = first_name
-            lead.last_name = last_name
-            lead.age = age
-            lead.save()
+            form.save()
             return redirect("/website")
     context = {
         'form': form,
         "lead": lead
     }
     return render(request, "website/lead_update.html", context)
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+#             return redirect("/website")
+#     context = {
+#         'form': form,
+#         "lead": lead
+#     }
+#     return render(request, "website/lead_update.html", context)
 
 
 # def lead_create(request):
@@ -67,9 +81,15 @@ def lead_create(request):
     return render(request, "website/lead_create.html", context)
 
 
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/website")
+
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {
         "lead": lead
     }
-    return render(request, "website/lead_update.html", context)
+    return render(request, "website/lead_detail.html", context)
