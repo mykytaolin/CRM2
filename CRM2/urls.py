c7822d65b7c9b7c86a15ee00e86d01be856f055f
mykytaolin <olin.mykyta@gmail.com>
2024-01-11 23:39:53 +0100

    Adding some basic style to the website

--- CRM2/urls.py
+++ CRM2/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from website.views import landing_page
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing-page'),
     path("website/", include("website.urls", namespace="website"))
 ]
