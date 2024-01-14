5283d6c7714f27c4b86fe125c8dc136ea0b9921f
mykytaolin <olin.mykyta@gmail.com>
2024-01-14 22:19:54 +0100

    Make views more simple and change the functions to django classes and set the static in the project

--- CRM2/urls.py
+++ CRM2/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from website.views import landing_page
+from website.views import landing_page, LandingPageView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing_page, name='landing-page'),
-    path("website/", include("website.urls", namespace="website"))
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path("website/", include("website.urls", namespace="website")),
 ]
+
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- website/views.py
+++ website/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Lead, Agent
 from .forms import LeadModelForm, LeadForm
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+class LandingPageView(TemplateView):
+    template_name = "landing.html"
 
 
 def landing_page(request):
     return render(request, "landing.html")
 
 
+class LeadListView(ListView):
+    template_name = "website/lead_list.html"
+    queryset = Lead.objects.all()  # this line equal to  leads = Lead.objects.all()
+    context_object_name = "leads"
+
+
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
-        "leads": leads
+        "object_list": leads
     }
     return render(request, "website/lead_list.html", context)
+
+
+class LeadUpdateView(UpdateView):
+    template_name = "website/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("website:lead-update")
 
 
 def lead_update(request, pk):
@@ -28,6 +48,56 @@ def lead_update(request, pk):
         "lead": lead
     }
     return render(request, "website/lead_update.html", context)
+
+
+class LeadDetailView(DetailView):
+    template_name = "website/lead_detail.html"
+    queryset = Lead.objects.all()  # this line equal to  leads = Lead.objects.all()
+    context_object_name = "lead"
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "website/lead_detail.html", context)
+
+
+class LeadCreateView(CreateView):
+    template_name = "website/lead_create.html"
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("website:lead-list")
+
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == "POST":
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/website")
+    context = {
+        'form': form
+    }
+    return render(request, "website/lead_create.html", context)
+
+
+class LeadDeleteView(DeleteView):
+    template_name = "website/lead_delete.html"
+    queryset = Lead.objects.all()
+
+    def get_success_url(self):
+        return reverse("website:lead-list")
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/website")
+
 
 # def lead_update(request, pk):
 #     lead = Lead.objects.get(id=pk)
@@ -70,30 +140,3 @@ def lead_update(request, pk):
 #       'form': LeadForm()
 #   }
 #   return render(request, "website/lead_create.html", context)
-
-
-def lead_create(request):
-    form = LeadModelForm()
-    if request.method == "POST":
-        form = LeadModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/website")
-    context = {
-        'form': form
-    }
-    return render(request, "website/lead_create.html", context)
-
-
-def lead_delete(request, pk):
-    lead = Lead.objects.get(id=pk)
-    lead.delete()
-    return redirect("/website")
-
-
-def lead_detail(request, pk):
-    lead = Lead.objects.get(id=pk)
-    context = {
-        "lead": lead
-    }
-    return render(request, "website/lead_detail.html", context)
