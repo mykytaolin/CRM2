92935f58ac983e02d0611d092cacbb0387edea4b
mykytaolin <olin.mykyta@gmail.com>
2024-01-15 19:02:49 +0100

    Create the sending email functionality

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from .models import Lead, Agent
 from .forms import LeadModelForm, LeadForm
@@ -70,6 +71,16 @@ class LeadCreateView(CreateView):
 
     def get_success_url(self):
         return reverse("website:lead-list")
+
+    def form_valid(self, form):
+        # To send email
+        send_mail(
+            subject="A lead has been created",
+            message="go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
 
 
 def lead_create(request):
